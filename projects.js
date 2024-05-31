e2835943f184382d0a6d32630e0860a238b7a8ae
CJPrince15 <167864262+CJPrince15@users.noreply.github.com>
2024-05-31 19:52:46 +0200

    Update projects.js

--- projects.js
+++ projects.js
@@ -2,8 +2,28 @@ const slider = document.querySelector('.slider');
 
 function activate(e) {
   const items = document.querySelectorAll('.item');
-  e.target.matches('.next') && slider.append(items[0])
-  e.target.matches('.prev') && slider.prepend(items[items.length-1]);
+  if (e.target.matches('.next')) {
+    slider.append(items[0]);
+  }
+  if (e.target.matches('.prev')) {
+    slider.prepend(items[items.length - 1]);
+  }
 }
 
-document.addEventListener('click',activate,false);
+document.addEventListener('click', function(event) {
+  if (event.target.matches('.next') || event.target.matches('.prev')) {
+    activate(event);
+  }
+});
+
+document.getElementById('open1').addEventListener('click', function() {
+  window.location.href = 'https://www.thecactusinn.com/';
+});
+
+document.getElementById('open2').addEventListener('click', function() {
+  window.location.href = 'https://nuvisionhighschool.com/';
+});
+
+document.getElementById('open3').addEventListener('click', function() {
+  window.location.href = 'https://codepen.io/CJPrince';
+});
